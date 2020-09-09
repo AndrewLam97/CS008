da0867964db0965d2274cb3195c16a55a780b6dd
Andrew <alam1997@gmail.com>
2020-09-08 23:33:22 -0700

    z_ files

---
+++ deque/deque/z_work_report.h
@@ -0,0 +1,26 @@
+/*
+ * Author: Andrew Lam
+ * Project: Deque
+ * Purpose: Double ended queue from linked list functions
+ * Notes: Work Report file.
+ */
+
+#ifndef Z_WORK_REPORT_H
+#define Z_WORK_REPORT_H
+/*
+    Features:
+        -Not Implemented: Did not implement iterators
+    
+        -Implemented: double ended queue functionality (push from either end,
+            pop from either end, access item from either end)
+
+        -Partly implemented: None
+
+    Bugs     : features that are implemented are bug-free.
+
+    Reflections:
+        popRear() gave me the most difficulty as I kept seg faulting due to not
+        updating my push functions to include the new beginning and end links.
+
+*/
+#endif // Z_WORK_REPORT_H
